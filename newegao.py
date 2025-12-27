2b9f9bcb1b4cd3a2b93ad12a8111d4c75a7d336e
xiaopangju <dududu233@outlook.com>
2025-12-27 21:59:41 +0800

    deleted some contents

--- newegao.py
+++ newegao.py
@@ -2,10 +2,6 @@
 import webbrowser
 import time
 url = 'https://www.bilibili.com/video/BV1GJ411x7h7/'
-input("你获得了一份大礼。敲击回车以领取！")
-input("首先，输入你的KLPBBS UID：")
-input("然后，给出你的用户名：")
-print("享受你的大礼吧！")
 
 time.sleep(5)
 
